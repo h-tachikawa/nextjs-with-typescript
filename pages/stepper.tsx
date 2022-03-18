c674055bfeef9d7c611ff90ba842239fc98483a4
h-tachikawa <sle.60e@gmail.com>
2022-03-19 03:37:00 +0900

    style: implement stepper page

---
+++ pages/stepper.tsx
@@ -0,0 +1,180 @@
+import { NextPage } from "next";
+import Box from "@mui/material/Box";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Step,
+  StepButton,
+  Stepper,
+} from "@mui/material";
+import { Fragment, useState, VFC } from "react";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { CancelOutlined, CheckCircleOutlined } from "@mui/icons-material";
+
+const steps = [
+  "Select campaign settings",
+  "Create an ad group",
+  "Create an ad",
+];
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+const StepperContainer: NextPage = () => {
+  const [activeStep, setActiveStep] = useState(0);
+  const [completed, setCompleted] = useState<{ [k: number]: boolean }>({});
+
+  const totalSteps = () => {
+    return steps.length;
+  };
+
+  const completedSteps = () => {
+    return Object.keys(completed).length;
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps();
+  };
+
+  const handleNext = () => {
+    const newActiveStep =
+      isLastStep() && !allStepsCompleted()
+        ? steps.findIndex((step, i) => !(i in completed))
+        : activeStep + 1;
+    setActiveStep(newActiveStep);
+    scrollToTop();
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStep = (step: number) => () => {
+    setActiveStep(step);
+  };
+
+  const handleComplete = () => {
+    const newCompleted = completed;
+    newCompleted[activeStep] = true;
+    setCompleted(newCompleted);
+    handleNext();
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+    setCompleted({});
+  };
+
+  const Content: VFC = () => (
+    <Fragment>
+      {[1, 2, 3].map((i) => (
+        <Card key={i} sx={{ my: 4 }}>
+          <CardContent>
+            <Typography variant="h5" gutterBottom sx={{ mt: 2 }}>
+              Step {activeStep + 1}
+            </Typography>
+            <Typography variant="body1">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Box sx={{ flexGrow: 1 }} />
+            <Button
+              variant="outlined"
+              size="small"
+              startIcon={<CheckCircleOutlined />}
+            >
+              Done
+            </Button>
+            <Button
+              variant="outlined"
+              size="small"
+              startIcon={<CancelOutlined />}
+              color="secondary"
+            >
+              Not Done
+            </Button>
+          </CardActions>
+        </Card>
+      ))}
+    </Fragment>
+  );
+
+  return (
+    <Box sx={{ width: "95%" }}>
+      <Stepper nonLinear activeStep={activeStep}>
+        {steps.map((label, index) => (
+          <Step key={label} completed={completed[index]}>
+            <StepButton color="inherit" onClick={handleStep(index)}>
+              {label}
+            </StepButton>
+          </Step>
+        ))}
+      </Stepper>
+      <div>
+        {allStepsCompleted() ? (
+          <Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              All steps completed - you are finished
+            </Typography>
+            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+              <Box sx={{ flex: "1 1 auto" }} />
+              <Button onClick={handleReset}>Reset</Button>
+            </Box>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <Content />
+            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+              <Button
+                color="inherit"
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+              >
+                Back
+              </Button>
+              <Box sx={{ flexGrow: 1 }} />
+              <Button onClick={handleNext} sx={{ mr: 1 }}>
+                Next
+              </Button>
+              {activeStep !== steps.length &&
+                (completed[activeStep] ? (
+                  <Typography
+                    variant="caption"
+                    sx={{ display: "inline-block" }}
+                  >
+                    Step {activeStep + 1} already completed
+                  </Typography>
+                ) : (
+                  <Button onClick={handleComplete}>
+                    {completedSteps() === totalSteps() - 1
+                      ? "Finish"
+                      : "Complete Step"}
+                  </Button>
+                ))}
+            </Box>
+          </Fragment>
+        )}
+      </div>
+    </Box>
+  );
+};
+
+export default StepperContainer;
