40afaac06fead67d31f091a4515662f99a0c8611
h-tachikawa <sle.60e@gmail.com>
2022-03-19 04:27:56 +0900

    wip: ドロワーのリンクをクリックした際に TOP に飛ぶようにした

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,11 +14,20 @@ import {
   IconButton,
   Toolbar,
   Grid,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
 } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import MenuIcon from "@mui/icons-material/Menu";
-import { AccountCircle } from "@mui/icons-material";
+import { AccountCircle, Inbox, Mail } from "@mui/icons-material";
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
@@ -40,8 +49,42 @@ export const themeOptions: ThemeOptions = {
 
 const theme = createTheme(themeOptions);
 
+type Props = {
+  open: boolean;
+  onClose: () => void;
+};
+
+export const TemporaryDrawer = (props: Props) => {
+  return (
+    <Drawer open={props.open} onClose={props.onClose}>
+      <Box sx={{ width: 300 }} role="presentation">
+        <List>
+          {["Inbox", "Starred", "Send email", "Top"].map((text, index) => (
+            <ListItem button key={text} onClick={props.onClose}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <Inbox /> : <Mail />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </Box>
+    </Drawer>
+  );
+};
+
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const toggleDrawer = () => setDrawerOpen(!drawerOpen);
+  const router = useRouter();
+
+  const handleClose = async () => {
+    toggleDrawer();
+    await router.push("/");
+  };
+
   return (
     <ApolloProvider client={client}>
       <CacheProvider value={emotionCache}>
@@ -59,11 +102,12 @@ export default function MyApp(props: MyAppProps) {
                 color="inherit"
                 aria-label="menu"
                 sx={{ mr: 2 }}
+                onClick={toggleDrawer}
               >
                 <MenuIcon />
               </IconButton>
               <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                My App
+                UR Rental Housing Visualizer
               </Typography>
               <IconButton
                 size="large"
@@ -77,6 +121,7 @@ export default function MyApp(props: MyAppProps) {
               </IconButton>
             </Toolbar>
           </AppBar>
+          <TemporaryDrawer open={drawerOpen} onClose={handleClose} />
           <Container>
             <Grid
               container
